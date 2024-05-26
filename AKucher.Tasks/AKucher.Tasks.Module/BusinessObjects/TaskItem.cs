9204d766f072b86501b96d3fa8fcc741f7bd1346
akucher <akucher@devpark.ru>
2024-05-26 11:09:51 +0300

    - Use bind

--- AKucher.Tasks/AKucher.Tasks.Module/BusinessObjects/TaskItem.cs
+++ AKucher.Tasks/AKucher.Tasks.Module/BusinessObjects/TaskItem.cs
@@ -2,43 +2,55 @@
 
 namespace AKucher.Tasks.Module.BusinessObjects
 {
-	public class TaskItem : BaseObject
-	{
-		public TaskItem(Session session) : base(session)
-		{
-		}
-
-		private uint order;
-
-		public uint Order
-		{
-			get => order;
-			set => SetPropertyValue(nameof(Order), ref order, value);
-		}
-
-		private string description;
-
-		public string Description
-		{
-			get => description;
-			set => SetPropertyValue(nameof(Description), ref description, value);
-		}
-
-		private bool completed;
-
-		public bool Completed
+    public class TaskItem : BaseObject
+    {
+        public TaskItem(Session session) : base(session)
         {
-			get => completed;
-			set => SetPropertyValue(nameof(Completed), ref completed, value);
-		}
+        }
 
-		private TaskObject task;
+        private uint order;
 
-		[Association(TaskObject.TaskObjectItemsAssociationName)]
-		public TaskObject Task
-		{
-			get => task;
-			set => SetPropertyValue(nameof(Task), ref task, value);
-		}
-	}
+        public uint Order
+        {
+            get => order;
+            set => SetPropertyValue(nameof(Order), ref order, value);
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get => description;
+            set => SetPropertyValue(nameof(Description), ref description, value);
+        }
+
+        private bool completed;
+
+        public bool Completed
+        {
+            get => completed;
+            set => SetPropertyValue(nameof(Completed), ref completed, value);
+        }
+
+        private TaskObject task;
+
+        [Association(TaskObject.TaskObjectItemsAssociationName)]
+        public TaskObject Task
+        {
+            get => task;
+            set => SetPropertyValue(nameof(Task), ref task, value);
+        }
+
+        protected override void OnChanged(string propertyName, object oldValue, object newValue)
+        {
+            base.OnChanged(propertyName, oldValue, newValue);
+
+            if (IsLoading || IsSaving)
+            {
+                return;
+            }
+
+            ((UnitOfWork)Session).CommitChanges();
+        }
+    }
 }
